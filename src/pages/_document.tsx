f226f8ebbca63952cb62d771bb0564c846a2ea39
jonaengs <jonaengs@gmail.com>
2024-04-17 22:52:57 +0200

    put navbar inside html body

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Navbar from '@/components/Navbar';
+
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head />
-      <Navbar />
       <body data-theme='sahara'>
         <Main />
         <NextScript />
